fe94e59c16234086d004a93a4e7ba2bf578cdc9c
Lev Erminson <erminson@gmail.com>
2021-04-09 14:10:37 +0300

    Implemented AccountService class

---
+++ src/main/java/accounts/AccountService.java
@@ -0,0 +1,34 @@
+package accounts;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AccountService {
+    private final Map<String, UserProfile> loginToProfile;
+    private final Map<String, UserProfile> sessionIdToProfile;
+
+    public AccountService() {
+        this.loginToProfile = new HashMap<>();
+        this.sessionIdToProfile = new HashMap<>();
+    }
+
+    public void addNewUser(final UserProfile userProfile) {
+        loginToProfile.put(userProfile.getLogin(), userProfile);
+    }
+
+    public UserProfile getUserByLogin(final String login) {
+        return loginToProfile.get(login);
+    }
+
+    public void addNewSessionId(final String sessionId, final UserProfile userProfile) {
+        sessionIdToProfile.put(sessionId, userProfile);
+    }
+
+    public UserProfile getUserBySessionId(final String sessionId) {
+        return sessionIdToProfile.get(sessionId);
+    }
+
+    public void deleteSession(final String sessionId) {
+        sessionIdToProfile.remove(sessionId);
+    }
+}
