e7a54a5a198f20797fead93a1638bd12f2a924f5
Lev Erminson <erminson@gmail.com>
2021-04-09 16:18:03 +0300

    Implemented logic SessionsServlet

--- src/main/java/servlets/SessionsServlet.java
+++ src/main/java/servlets/SessionsServlet.java
@@ -1,6 +1,8 @@
 package servlets;
 
 import accounts.AccountService;
+import accounts.UserProfile;
+import com.google.gson.Gson;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -17,13 +19,56 @@ public class SessionsServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws ServletException, IOException {
+        String sessionId = request.getSession().getId();
+        UserProfile userProfile = accountService.getUserBySessionId(sessionId);
+        if (userProfile == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        } else {
+            Gson gson = new Gson();
+            String json = gson.toJson(userProfile);
+            response.containsHeader("text/html;charset=utf-8");
+            response.getWriter().println(json);
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
     }
 
     public void doPost(HttpServletRequest request,
                       HttpServletResponse response) throws ServletException, IOException {
+        String login = request.getParameter("login");
+        String pass = request.getParameter("pass");
+
+        response.setContentType("text/html;charset=utf-8");
+        if (login == null || pass == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        final UserProfile userProfile = accountService.getUserByLogin(login);
+        if (userProfile == null || !userProfile.getPass().equals(pass)) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        final String sessionId = request.getSession().getId();
+        accountService.addNewSessionId(sessionId, userProfile);
+        Gson gson = new Gson();
+        String json = gson.toJson(userProfile);
+        response.getWriter().println(json);
+        response.setStatus(HttpServletResponse.SC_OK);
     }
 
     public void doDelete(HttpServletRequest request,
                       HttpServletResponse response) throws ServletException, IOException {
+        String sessionId = request.getSession().getId();
+        UserProfile userProfile = accountService.getUserBySessionId(sessionId);
+        response.setContentType("text/html;charset=utf-8");
+        if (userProfile == null) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        } else {
+            accountService.deleteSession(sessionId);
+            response.getWriter().println("Goodbye!");
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
     }
 }
