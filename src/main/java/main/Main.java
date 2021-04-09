348695e591ec6064a9925cc5a62f824c067c0d30
Lev Erminson <erminson@gmail.com>
2021-04-09 16:17:07 +0300

    Changes http servlets set for server and added new resource handler

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,21 +1,36 @@
 package main;
 
+import accounts.AccountService;
+import accounts.UserProfile;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-
-import servlets.AllRequestsServlet;
-import servlets.MirrorRequestsServlet;
+import servlets.SessionsServlet;
+import servlets.UsersServlet;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
-        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
-        MirrorRequestsServlet mirrorRequestsServlet = new MirrorRequestsServlet();
+
+        AccountService accountService = new AccountService();
+
+        accountService.addNewUser(new UserProfile("admin"));
+        accountService.addNewUser(new UserProfile("test"));
+
+        SessionsServlet sessionsServlet = new SessionsServlet(accountService);
+        UsersServlet usersServlet = new UsersServlet(accountService);
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
-        context.addServlet(new ServletHolder(mirrorRequestsServlet), "/mirror");
+        context.addServlet(new ServletHolder(sessionsServlet), "/api/v1/sessions");
+        context.addServlet(new ServletHolder(usersServlet), "/api/v1/users");
+
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] { resourceHandler, context });
 
         Server server = new Server(8080);
         server.setHandler(context);
