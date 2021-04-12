399243b0b00d99d936b23f524e8f10ff3d52a9ad
Lev Erminson <erminson@gmail.com>
2021-04-12 08:28:02 +0300

    Implemented servlet SignInServlet

--- src/main/java/servlets/SignInServlet.java
+++ src/main/java/servlets/SignInServlet.java
@@ -1,2 +1,39 @@
-package servlets;public class SignInServlet {
+package servlets;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SignInServlet extends HttpServlet {
+    private final AccountService accountService;
+
+    public SignInServlet(final AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+        final String login = request.getParameter("login");
+        final String pass = request.getParameter("password");
+
+        response.setContentType("text/html;charset=utf-8");
+        if (login == null || pass == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        final UserProfile userProfile = accountService.getUserByLogin(login);
+        if (userProfile == null) {
+            response.getWriter().println("Unauthorized");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        } else {
+            response.getWriter().println("Authorized: " + login);
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+    }
 }
